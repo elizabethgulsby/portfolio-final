00ac4fd5cdb80c91c6d105d8529cfa4f2f0e6549
Elizabeth Gulsby <elizabeth.gulsby@gmail.com>
2017-02-23 19:29:30 -0500

    Init commit; refactored initial porfolio, created final master repository

---
+++ assets/js/custom.js
@@ -0,0 +1,92 @@
+(function($){
+
+	/* ---------------------------------------------- /*
+	 * Preloader
+	/* ---------------------------------------------- */
+
+	$(window).load(function() {
+		$('#status').fadeOut();
+		$('#preloader').delay(300).fadeOut('slow');
+	});
+
+	$(document).ready(function() {
+
+		/* ---------------------------------------------- /*
+		 * Smooth scroll / Scroll To Top
+		/* ---------------------------------------------- */
+
+		$('a[href*=#]').bind("click", function(e){
+           
+			var anchor = $(this);
+			$('html, body').stop().animate({
+				scrollTop: $(anchor.attr('href')).offset().top
+			}, 1000);
+			e.preventDefault();
+		});
+
+		$(window).scroll(function() {
+			if ($(this).scrollTop() > 100) {
+				$('.scroll-up').fadeIn();
+			} else {
+				$('.scroll-up').fadeOut();
+			}
+		});
+
+		/* ---------------------------------------------- /*
+		 * Navbar
+		/* ---------------------------------------------- */
+
+		$('.header').sticky({
+			topSpacing: 0
+		});
+
+		$('body').scrollspy({
+			target: '.navbar-custom',
+			offset: 70
+		})
+
+		// $('#body').scrollspy({
+		// 	target: '#Skills'
+		// 	offset: 70
+		// })
+		
+        
+		/* ---------------------------------------------- /*
+		 * Home BG
+		/* ---------------------------------------------- */
+
+		$(".screen-height").height($(window).height());
+
+		$(window).resize(function(){
+			$(".screen-height").height($(window).height());
+		});
+
+		if (/Android|webOS|iPhone|iPad|iPod|BlackBerry/i.test(navigator.userAgent)) {
+			$('#home').css({'background-attachment': 'scroll'});
+		} else {
+			$('#home').parallax('50%', 0.1);
+		}
+
+
+		/* ---------------------------------------------- /*
+		 * WOW Animation When You Scroll
+		/* ---------------------------------------------- */
+
+		wow = new WOW({
+			mobile: false
+		});
+		wow.init();
+
+		/* ---------------------------------------------- /*
+		 * Repo/Demo buttons appearing on hover
+		/* ---------------------------------------------- */
+
+		$(document).on('mouseenter', '.grid', function () {
+                    $(this).find(":button").show();
+                }).on('mouseleave', '.grid', function () {
+                    $(this).find(":button").hide();
+                });
+
+	});
+
+})(jQuery);
